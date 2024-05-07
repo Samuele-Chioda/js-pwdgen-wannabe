60d9dc37fcd9b6858919a7c0bfe5392ae7489130
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-07 13:21:16 +0200

    fine

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let nomeUtente = prompt("ciao, inserisci il tuo nome!");
+let cognomeUtente = prompt("ciao, inserisci il tuo cognome!");
+let colorePreferitoUtente = prompt("ciao, inserisci il tuo colore preferito!");
+
+document.getElementById("output").innerHTML =nomeUtente + cognomeUtente + colorePreferitoUtente + "24";
